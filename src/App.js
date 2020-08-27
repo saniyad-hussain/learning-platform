63a5b5a89d05630c81245f089461ba81a75891f7
saniyad-hussain <chowdhurysaniayd@gmail.com>
2020-08-27 00:08:17 -0400

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import fakeData from './fakeData/courses';
 import Main from './components/Main/Main';
 import { useState } from 'react';
+import Header from './components/Header/Header';
 
 function App() {
   const [courses, setCourses] = useState(fakeData);
 
   return (
-    <div className="container">
-      <div className="row">
+    <div>
+      <Header></Header>
+      <div className="container">
+       <div className="row">
         {
           courses.map(course => 
           <div className="col-md-4">
             <Main course={course}></Main>
           </div>)
         }
+       </div>
       </div>
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-light bg-dark">
+                <h2 className="text-primary">Kudemy</h2>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
