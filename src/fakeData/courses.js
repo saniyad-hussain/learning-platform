af6a14b254bf39fcb03313939c571c1e6de678fe
saniyad-hussain <chowdhurysaniayd@gmail.com>
2020-08-26 23:40:15 -0400

    fake data added

---
+++ src/fakeData/courses.js
@@ -0,0 +1,94 @@
+const courses = [
+    {
+        name: 'Build Responsive Real World Websites with HTML5 and CSS3',
+        instructor: 'Itamar Rosenblum',
+        price: 109.99,
+        image: 'https://img-a.udemycdn.com/course/240x135/437398_46c3_9.jpg',
+    },
+    {
+        name: 'Python and Django Full Stack Web Developer Bootcamp',
+        instructor: 'Donald K',
+        price: 200.99,
+        image: 'https://img-a.udemycdn.com/course/240x135/822444_a6db.jpg',
+    },
+    {
+        name: 'Modern JavaScript From The Beginning',
+        instructor: 'Brad Traversy',
+        price: 309.99,
+        image: 'https://img-a.udemycdn.com/course/240x135/1463348_52a4_2.jpg',
+    },
+    {
+        name: 'PHP for Beginners - Become a PHP Master - CMS Project',
+        instructor: 'Sergio Sanchez',
+        price: 9.99,
+        image: 'https://img-a.udemycdn.com/course/240x135/405282_27d2.jpg',
+    },
+    {
+        name: 'The Advanced Web Developer Bootcamp',
+        instructor: 'Colt Steel',
+        price: 19.99,
+        image: 'https://img-a.udemycdn.com/course/240x135/1218586_9f86.jpg',
+    },
+    {
+        name: 'Complete React Developer in 2020 (w/ Redux, Hooks, GraphQL)',
+        instructor: 'Ashfaq Shimer',
+        price: 39.99,
+        image: 'https://img-a.udemycdn.com/course/240x135/2365628_0b60_7.jpg',
+    },
+    {
+        name: 'Node.js, Express, MongoDB & More: The Complete Bootcamp 2020',
+        instructor: 'vikrant gupta',
+        price: 35.99,
+        image: 'https://img-a.udemycdn.com/course/240x135/2201164_831a.jpg',
+    },
+    {
+        name: 'Become a WordPress Developer: Unlocking Power With Code',
+        instructor: 'Jeffrey Lee',
+        price: 55.99,
+        image: 'https://img-a.udemycdn.com/course/240x135/1010586_b622_3.jpg',
+    },
+    {
+        name: 'iOS 13 & Swift 5 - The Complete iOS App Development Bootcamp',
+        instructor: 'Dr. Angela Yu',
+        price: 105.99,
+        image: 'https://img-a.udemycdn.com/course/240x135/1778502_f4b9_11.jpg',
+    },
+    {
+        name: 'ClickBank Success – Affiliate Marketing Without A Website',
+        instructor: 'KC Tan',
+        price: 25.99,
+        image: 'https://img-a.udemycdn.com/course/240x135/313360_0668_3.jpg',
+    },
+    {
+        name: 'Instagram Marketing 2020: Hashtags, Live, Stories, Ads &more',
+        instructor: 'Evan Kimbrell',
+        price: 15.99,
+        image: 'https://img-a.udemycdn.com/course/240x135/1692684_682b.jpg',
+    },
+    {
+        name: 'Bootstrap Real-time Project in Just 3 hr - From Scratch',
+        instructor: 'Harsha Vardhan',
+        price: 175.99,
+        image: 'https://img-a.udemycdn.com/course/240x135/398456_aefd_4.jpg',
+    },
+    {
+        name: 'Create a PHP Contact Form in Bootstrap',
+        instructor: 'Andy Clarke',
+        price: 250.99,
+        image: 'https://img-a.udemycdn.com/course/240x135/654422_48b1_2.jpg',
+    },
+    {
+        name: 'ClickBank Success: Zero Cost Advertising Affiliate Marketing',
+        instructor: 'KC Tan',
+        price: 150.99,
+        image: 'https://img-a.udemycdn.com/course/240x135/630050_7fc3.jpg',
+    },
+    {
+        name: 'Passive Income - Amazon Affiliate Marketing Websites / Blogs',
+        instructor: 'Dushyant Bhatia',
+        price: 40.99,
+        image: 'https://img-a.udemycdn.com/course/240x135/3090420_9eb7_3.jpg',
+    },
+]
+
+export default courses;
